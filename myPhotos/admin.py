61b1af313a426f958bc1b3abba5e7382618aea3f
deibele1 <deibele1@gmail.com>
2022-03-21 00:01:54 -0400

    Registered untagged photo

--- myPhotos/admin.py
+++ myPhotos/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import DogPhoto, LitterPhoto, PuppyPhoto, CaretakerPhoto
+from .models import Photo, DogPhoto, LitterPhoto, PuppyPhoto, CaretakerPhoto
 
+admin.site.register(Photo)
 admin.site.register(DogPhoto)
 admin.site.register(LitterPhoto)
 admin.site.register(PuppyPhoto)
