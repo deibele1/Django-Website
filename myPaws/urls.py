82e01f8bfbae66f9aa3b1c38cf0fe61dd76dbb16
deibele1 <deibele1@gmail.com>
2022-03-22 22:29:36 -0400

    Fixed bug

--- myPaws/urls.py
+++ myPaws/urls.py
@@ -6,7 +6,7 @@ from myPaws.views import DogDetailView, LitterDetailView, PuppyDetailView, DogIn
 urlpatterns = [
     path('', RedirectView.as_view(url='index', permanent=True)),
     path('index', DogIndexView.as_view(), name='bigPawIndex'),
-    path('litters', LitterIndexView.as_view, name='littersIndex'),
+    path('litters', LitterIndexView.as_view(), name='littersIndex'),
     path('<name>', DogDetailView.as_view(), name='bigPaw'),
     path('littlePaws/<id>', LitterDetailView.as_view(), name='littlePaws'),
     path('littlePaws/<id>/<name>', PuppyDetailView.as_view(), name='littlePaw'),
